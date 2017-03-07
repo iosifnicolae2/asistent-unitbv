99607dc0ecba3bf86985e20f12f2eed684120ece
Iosif Nicolae <iosifnicolae2@gmail.com>
2017-03-07 13:46:30 +0200

    ok

--- routes/index.js
+++ routes/index.js
@@ -95,43 +95,7 @@ function sendOrarMessage(recipientId, messageText) {
       id: recipientId
     },
     message: {
-      text: `Orarul dumneavoastra de astazi este: \n Marti - 07.02.2017 \n Seminar matematici speciale - 16:00 VII12\n Seminar matematici speciale - 16:00 VII12\n <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-</table>`
+      text: `Orarul dumneavoastra de astazi este: \n Marti - 07.02.2017 \n Seminar matematici speciale - 16:00 VII12\n Seminar matematici speciale - 16:00 VII12\n`
     }
   };
 
